3d491239f9737657463f86bb2d91193bd89e24c0
dahlbaek <30782351+dahlbaek@users.noreply.github.com>
2026-08-11 16:31:29 +0200

    Support countDistinct aggregate (#217)
    This adds support for distinct count aggregate. The implementation is
    straight forward except for that BigQuery does not support distinct
    aggregates on structs and arrays. So in order to support BigQuery a
    rewrite rule is added that rewrite such quries to use `SELECT DISTINCT`
    and the normal count and join the result when there are multiple
    aggregates.
    
    This translation might not be optimal as join are quite expensive. In
    some future iteration we might want to look closer at how Spark rewrites
    it internally
    https://github.com/apache/spark/blob/50514c5271e0fae3f2546c4edea9da8ee3323344/sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/optimizer/RewriteDistinctAggregates.scala
    But it not clear that possible to do on BigQuery as it uses per
    aggregate FILTER clause, which I don't think BigQuery currently
    supports.
    
    ---------
    
    Co-authored-by: Emil Ejbyfeldt <emil.ejbyfeldt@choreograph.com>

--- tyda-test-suites/src/main/scala/com/choreograph/tyda/testsuites/DatasetAggregatesSuite.scala
+++ tyda-test-suites/src/main/scala/com/choreograph/tyda/testsuites/DatasetAggregatesSuite.scala
@@ -20,6 +20,7 @@ import com.choreograph.tyda.aggregates.boolOr
 import com.choreograph.tyda.aggregates.collect
 import com.choreograph.tyda.aggregates.concat
 import com.choreograph.tyda.aggregates.count
+import com.choreograph.tyda.aggregates.countDistinct
 import com.choreograph.tyda.aggregates.countIf
 import com.choreograph.tyda.aggregates.countSome
 import com.choreograph.tyda.aggregates.max
@@ -163,6 +164,31 @@ trait DatasetAggregatesSuite extends DatasetSuite {
   test[Option[Int], Long](
     "countSome whole row",
     ds => ds.groupByKey(_ => lit(1)).aggregateValue(countSome).values
+  )
+  test[Int, Long]("countDistinct whole row", ds => ds.groupByKey(_ => 1).aggregateValue(countDistinct).values)
+  test[(Int, Int), (Int, Long)](
+    "countDistinct group by",
+    ds => ds.groupByKey(_._1).aggregateValue(countDistinct(_._2)).pairs
+  )
+  test[Pair, Long]("countDistinct struct", ds => ds.groupByKey(_ => 1).aggregateValue(countDistinct).values)
+  test[(Pair, Pair), (key: TinyByte, d1: Long, d2: Long)](
+    "countDistinct struct multiple",
+    ds => ds.groupByKey(_._1._1).aggregate(r => (d1 = countDistinct(r._1), d2 = countDistinct(r._2)))
+  )
+  test[(Pair, Pair), (key: TinyByte, d1: Long, d2: Long, tot: Long)](
+    "countDistinct struct multiple and supported",
+    ds =>
+      ds.groupByKey(_._1._1)
+        .aggregate(r => (d1 = countDistinct(r._1), d2 = countDistinct(r._2), tot = count(r)))
+  )
+  test[(Pair, Pair), Option[(d1: Long, d2: Long, tot: Long)]](
+    "countDistinct struct multiple and supported ungrouped",
+    ds => ds.aggregate(r => (d1 = countDistinct(r._1), d2 = countDistinct(r._2), tot = count(r)))
+  )
+  test[Pair, Option[Long]]("countDistinct struct ungrouped", ds => ds.aggregate(countDistinct))
+  test[Seq[Int], Long](
+    "countDistinct array",
+    ds => ds.groupByKey(_ => 1).aggregateValue(countDistinct).values
   )
   test[(TinyByte, Option[Int]), Long](
     "countSome expr",
