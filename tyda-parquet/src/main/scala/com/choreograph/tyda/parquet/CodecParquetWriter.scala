26d3f24983a1df17b392b2a4a60cb9568a72ea9f
dahlbaek <30782351+dahlbaek@users.noreply.github.com>
2026-08-13 10:50:10 +0200

    Remove usage of deprecated constructor (#229)
    The ParquetWriter.Builder(Path path) constructur gets deprecated in
    https://github.com/apache/parquet-java/pull/3301, which is in release
    1.17 of parquet-hadoop:
    
    https://github.com/apache/parquet-java/releases#release-apache-parquet-1.17.0

--- tyda-parquet/src/main/scala/com/choreograph/tyda/parquet/CodecParquetWriter.scala
+++ tyda-parquet/src/main/scala/com/choreograph/tyda/parquet/CodecParquetWriter.scala
@@ -3,13 +3,16 @@ package com.choreograph.tyda.parquet
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.Path
 import org.apache.parquet.hadoop.ParquetWriter
+import org.apache.parquet.hadoop.util.HadoopOutputFile
 
 import com.choreograph.tyda.Codec
 
 object CodecParquetWriter {
   def apply[T: Codec](path: Path): ParquetWriter[T] = new Builder[T](path).build()
 
-  final class Builder[T: Codec](path: Path) extends ParquetWriter.Builder[T, Builder[T]](path) {
+  private def fromPath(path: Path): HadoopOutputFile = HadoopOutputFile.fromPath(path, new Configuration())
+
+  final class Builder[T: Codec](path: Path) extends ParquetWriter.Builder[T, Builder[T]](fromPath(path)) {
     override protected def self(): Builder[T] = this
 
     override protected def getWriteSupport(conf: Configuration): CodecWriteSupport[T] =
